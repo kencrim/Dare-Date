2749141481fb3dbb008cf6be41781df07bbb6877
dyliman <dyyliman@gmail.com>
2019-03-12 12:45:32 -0700

    dares will persist even after navigating away from the dare list

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,43 @@ import AboutPage from '../components/about/AboutPage';
 import DareList from '../components/dare/DareList';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dareList: []
+    };
+    this.dareComplete = this.dareComplete.bind(this)
+    this.dareFail = this.dareFail.bind(this)
+    this.addDare = this.addDare.bind(this)
+  }
+
+  dareComplete(dare) {
+    dare.complete = true;
+    this.setState({dareList: this.state.dareList})
+  }
+
+  dareFail(dare) {
+    dare.fail = true;
+    this.setState({dareList: this.state.dareList})
+  }
+
+  addDare(dare, bool, strike, timer, timerTime, repeat, maxStrikes) {
+    let currentTime = new Date;
+    let newDare = {bool: true, strikes: true, timer: true, timerTimeMins: 1, repeat: false, maxStrikes: 3, dare: "This is the dare", complete: false, fail: false, timeCreated: currentTime}
+    this.state.dareList.push(newDare)
+    this.setState({dareList: this.state.dareList});
+  }
+
 	render(){
 		return (
 			<div className="container-fluid">
+				<button onClick = {this.addDare}>Add Dare</button>
 				<Header/>
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/about" component={AboutPage} />
-					<Route path="/dare" component={DareList} />
+					<Route path="/dare" render={() => <DareList dareList={this.state.dareList} complete={this.dareComplete} fail={this.dareFail} />}/>
 				</Switch>
 			</div>
 			);
